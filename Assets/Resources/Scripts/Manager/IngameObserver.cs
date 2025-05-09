d1ae5cc70af46098e2c55795735693f4d5e3653e
aruensia <aruensia@naver.com>
2025-05-09 11:16:39 +0900

    [Add] 프레임워크 메소드 추가.

--- Assets/Resources/Scripts/Manager/IngameObserver.cs
+++ Assets/Resources/Scripts/Manager/IngameObserver.cs
@@ -3,35 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IngameObserver : MonoBehaviour
+public class IngameObserver
 {
-    public event Action<DataCenter> OnGameDataChange;
+    public event Action<int> OnGameDataChange;
+    public event Action OnGameEnd;
 
     public UserPlayer UserPlayer { get; private set; }
+    private int gameoverTargetScore = 100;
     private DataCenter gamedata = new DataCenter(3, 0);
+    private bool isGameOver = false;
 
-    public void ResetData()
+
+    public void HitPlayer(int damage)
     {
-        gamedata = new DataCenter(3, 0);
-        OnGameDataChange?.Invoke(gamedata.Clone());
+        gamedata.life = gamedata.life - damage;
+        var templife = gamedata.life;
+
+        //여기에 포톤 추가.
+        OnGameDataChange?.Invoke(templife);
+
+        if (gamedata.life <= 0)
+        {
+            isGameOver = true;
+            OnGameEnd.Invoke();
+        }
     }
 
-    public void HitPlayer()
+    public void AddScore(int score)
     {
-        --gamedata.life;
-        var tempGameData = gamedata.Clone();
-        OnGameDataChange?.Invoke(tempGameData);
+        gamedata.score = gamedata.score + score;
+        var tempscore = gamedata.score;
+
+        //여기에 포톤 추가.
+        OnGameDataChange?.Invoke(tempscore);
+
+        if(gamedata.score >= gameoverTargetScore)
+        {
+            isGameOver = true;
+            OnGameEnd.Invoke();
+        }
     }
 
-    public void AddScore()
+    public void ResetPlayer()
     {
-        ++gamedata.score;
-        var tempGameData = gamedata.Clone();
-        OnGameDataChange?.Invoke(tempGameData);
+
     }
 
     public void EndGame()
     {
-
+        if(isGameOver == true)
+        {
+            OnGameEnd.Invoke();
+        }
     }
 }
