dbee3566714114378176f79d5f8b6be93dd15015
aruensia <aruensia@naver.com>
2025-05-16 14:22:44 +0900

    [Add] Item cs 추가.

--- Assets/Resources/Scripts/Data/DataCenter.cs
+++ Assets/Resources/Scripts/Data/DataCenter.cs
@@ -19,14 +19,21 @@ public class DataCenter
     public int score;
     public FairyType playerFairyType;
 
-    public DataCenter(DataCenter gamedata) : this(gamedata.life, gamedata.score) { }
+    public DataCenter(DataCenter gamedata) : this(gamedata.life, gamedata.score, gamedata.playerFairyType) { }
+
+    public DataCenter(int initLife, int initSocre, FairyType initfairy)
+    {
+        life = initLife;
+        score = initSocre;
+        playerFairyType = initfairy;
+    }
 
     public DataCenter(int initLife, int initSocre)
     {
         life = initLife;
         score = initSocre;
     }
- 
+
     public DataCenter Clone()
     {
         return new DataCenter(this);
