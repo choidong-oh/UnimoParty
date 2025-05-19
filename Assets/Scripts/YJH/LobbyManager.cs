b43851f43ba7e36f28f68e6f7448850a83aa6892
jonghyun109 <190792801+jonghyun109@users.noreply.github.com>
2025-05-19 17:53:31 +0900

    [feat] 로비 유저 정보 받아오기

--- Assets/Scripts/YJH/LobbyManager.cs
+++ Assets/Scripts/YJH/LobbyManager.cs
@@ -1,19 +1,78 @@
-using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
-    public Button makeRoom;
+    public Transform contentParent;
+    public GameObject userButtonPrefab;
+
+    private Dictionary<string, GameObject> playerButtons = new Dictionary<string, GameObject>();
+
     void Start()
     {
+        PhotonNetwork.NickName = FirebaseLoginMgr.user.DisplayName;
         PhotonNetwork.ConnectUsingSettings();
     }
-    private void OnConnectedToServer()
+
+    public override void OnConnectedToMaster()
     {
+        PhotonNetwork.JoinRandomRoom();
     }
 
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        RoomOptions options = new RoomOptions { MaxPlayers = 20 };
+        PhotonNetwork.CreateRoom("LobbyRoom", options, TypedLobby.Default);
+    }
 
+    public override void OnJoinedRoom()
+    {
+        RefreshPlayerList();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        AddPlayerButton(newPlayer);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        RemovePlayerButton(otherPlayer);
+    }
+
+    void RefreshPlayerList()
+    {
+        foreach (var btn in playerButtons.Values)
+        {
+            Destroy(btn);
+        }
+        playerButtons.Clear();
+
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            AddPlayerButton(p);
+        }
+    }
+
+    void AddPlayerButton(Player p)
+    {
+        if (playerButtons.ContainsKey(p.NickName))
+            return;
+
+        GameObject button = Instantiate(userButtonPrefab, contentParent);
+        button.GetComponentInChildren<Text>().text = p.NickName;
+        playerButtons.Add(p.NickName, button);
+    }
+
+    void RemovePlayerButton(Player p)
+    {
+        if (playerButtons.TryGetValue(p.NickName, out GameObject btn))
+        {
+            Destroy(btn);
+            playerButtons.Remove(p.NickName);
+        }
+    }
 }
