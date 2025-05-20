36efc89cb66c2c4974fb688a823527dd49f791e4
choidong-oh <qltjfeo@naver.com>
2025-05-20 23:35:18 +0900

    [ADD] 기존 생성코드 풀링으로 전환

--- Assets/Scripts/CDO/EnemyScript/EnemySpawnerCommand.cs
+++ Assets/Scripts/CDO/EnemyScript/EnemySpawnerCommand.cs
@@ -19,6 +19,12 @@ public class EnemySpawnerCommand : MonoBehaviour
         // 적의 종류에 따라 프리팹과 커맨드 설정
         if (enemyType == "Burnduri")
         {
+            //이거같은데? 
+            //근데 좀 애매한게 이러면 하나의 프리팹만 가능하지안나?
+            //이거는 내가 예전에 하다 포기한거 해야될듯? 프리팹 다른애들도 풀링하게 
+            //물어봐야댐 아닐수있음
+            //var enemyObject1 = EnemyPool.enemypool.GetEnemy();
+
             enemyObject = Instantiate(burnduriEnemyPrefab, transform.position, Quaternion.identity);
             command = new MoveCommand(enemyObject, direction, speed);
             command.Execute();
