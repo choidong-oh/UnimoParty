1f8e808b911505946770c092b8e77ffe22a34c0e
choidong-oh <qltjfeo@naver.com>
2025-05-21 22:54:43 +0900

    [ADD] 적 생성 기능 추가함 ,플레이어 위치 생성 예외코드 추가함

--- Assets/Scripts/CDO/EnemyScript/BurnduriSpawn.cs
+++ Assets/Scripts/CDO/EnemyScript/BurnduriSpawn.cs
@@ -1,4 +1,3 @@
-using Unity.Mathematics;
 using UnityEngine;
 
 public class BurnduriSpawn : MonoBehaviour
@@ -6,57 +5,84 @@ public class BurnduriSpawn : MonoBehaviour
     public EnemySpawnerCommand enemySpawnerCommand;
     public GameObject testPrefab;
     Vector3 spawnPosition;
-    
+
+    [SerializeField] Transform player;
+    [SerializeField] Transform donutTransform;
+    [SerializeField] float distanceGap; 
+    float angle; // 각도
+    int r = 15; //반지름
 
     private void Start()
     {
-        InvokeRepeating("Spawn", 1f, 2f);
-        Spawn();
-        Spawn();
-        Spawn();
-        Spawn();
-        Spawn();
-        Spawn();
-        Spawn();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        InvokeRepeating("SpawnEnemy", 1f, 2f);
     }
 
-    void SpawnOneEnemy()
+
+    void SpawnEnemy()
     {
-        spawnPosition = new Vector3(10,20,30);
 
-        var pl =  Instantiate(testPrefab);
-        pl.transform.position = GetRandomPosition(20);
-        //오브, player 안되게
-        //var tempEnemy = enemySpawnerCommand.SpawnEnemy("Burnduri", GetRandomPosition(20), 5);
-        //if (tempEnemy.gameObject.activeInHierarchy)
-        //{
-
-        //}
+        //enemySpawnerCommand.SpawnEnemy("Burnduri", isPlayerHere(), 5);
+        enemySpawnerCommand.SpawnEnemy("Burnduri", IsDoWhile(), 5);
+        enemySpawnerCommand.SpawnEnemy("Burnduri", IsDoWhile(), 5);
+        enemySpawnerCommand.SpawnEnemy("Burnduri", IsDoWhile(), 5);
+        enemySpawnerCommand.SpawnEnemy("Burnduri", IsDoWhile(), 5);
+        enemySpawnerCommand.SpawnEnemy("Burnduri", IsDoWhile(), 5);
 
     }
 
-
-    void Spawn()
+    Vector3 DonutPostion()
     {
-        SpawnOneEnemy();
-        SpawnOneEnemy();
-        SpawnOneEnemy();
-        SpawnOneEnemy();
-        SpawnOneEnemy();
-        SpawnOneEnemy();
-        SpawnOneEnemy();
+        angle = UnityEngine.Random.Range(0, 360);
+        float x = Mathf.Cos(angle) * r;
+        float y = donutTransform.transform.position.y;
+        float z = Mathf.Sin(angle) * r;
+        donutTransform.position = new Vector3(x, y, z);
+
+        return donutTransform.position;
     }
 
-    public Vector3 GetRandomPosition(float radius)
+    Vector3 isPlayerHere()
     {
-        float x = UnityEngine.Random.Range(-1f, 1f);
-        float temp = Mathf.Pow(1, 2) - Mathf.Pow(x, 2);
-        float z = Mathf.Sqrt(temp);
+        for (int i = 0; i < 5; i++)
+        {
+            //var dd = donutTransform.position; //테스트버전
+            var dd = DonutPostion(); //실제
 
-        
-        return (new Vector3(x,0,z)* UnityEngine.Random.Range(0,radius)
-            + new Vector3(transform.position.x,0,transform.position.z));    
+            if (Vector3.Distance(dd, player.position) >= distanceGap)
+            {
+                Debug.Log("플레이어가 근처 아님ㅋ");
+                return dd;
+            }
+            else
+            {
+                Debug.Log("플레이어가 근처임");
+            }
+        }
+        //나중에 player근처 아닐때까지 돌리면될듯
+        Debug.Log("i번 돌렷는데 플레이어가 근처임");
+
+        var aa = DonutPostion();
+        return aa;
     }
+
+
+    Vector3 IsDoWhile()
+    {
+        Vector3 dd;
+        int roof = 0;
+        do 
+        { dd = DonutPostion();roof++; } 
+        while ((Vector3.Distance(dd, player.position) >= distanceGap)&&roof>=10);
+
+        if (roof >= 10) { Debug.Log("무한루프될뻔"); }
+
+        return dd;
+    }
+
+
+
 
 
 
