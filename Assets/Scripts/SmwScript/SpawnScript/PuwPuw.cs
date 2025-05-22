c34e1f7442b8eb4035862737780ccd37da181320
ldeemol <130666760+ldeemol@users.noreply.github.com>
2025-05-22 14:08:19 +0900

    [Add] 번드리 에셋에 스크립트 적용완료
    번드리 에셋에 스크립트 적용완료

--- Assets/Scripts/SmwScript/SpawnScript/PuwPuw.cs
+++ Assets/Scripts/SmwScript/SpawnScript/PuwPuw.cs
@@ -2,27 +2,36 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PuwPuw : MonoBehaviour
+public class PuwPuw : EnemyBase
 {
     Vector3 Postion;
 
     float Radius;
-    public float fixedY = 0f;
-    float MoveSpeed = 10f;
+    float fixedY;
+    float MoveSpeed;
     float Angle;           
     int rotateDirection;   //어디로갈지 시계 반시계
 
     Coroutine rotateCoroutine;
 
-    void OnEnable()
+    private void Start()
+    {
+        MoveSpeed = enemyMoveSpeed;
+    }
+
+    public override void Move(Vector3 direction)
     {
         Postion = transform.position;
+
+        int RandomScale = Random.Range(1, 4);
+        transform.localScale = new Vector3(RandomScale, RandomScale, RandomScale);
+        //fixedY = 
 
         //랜덤 각도에서 시작
         Angle = Random.Range(0f, Mathf.PI * 2f);
 
         //반지름 위치 
-        Radius = Random.Range(3f,20f);
+        Radius = Random.Range(3f, 20f);
 
         //랜덤 회전 방향(1 or -1)
         rotateDirection = Random.value < 0.5f ? 1 : -1;
@@ -30,6 +39,13 @@ public class PuwPuw : MonoBehaviour
         //원을 따라 도는 코루틴 시작
         rotateCoroutine = StartCoroutine(GoPewPew());//굳이 변수 선언한건 값 초기화 때문
     }
+
+    public override void CsvEnemyInfo()
+    {
+
+    }
+
+
 
     IEnumerator GoPewPew()
     {
