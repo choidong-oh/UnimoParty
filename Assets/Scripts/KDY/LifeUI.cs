43ec81bb1ef45cc582a113e0ec96098fa760e1ce
KIMDONGYEON8406 <kdys8599@naver.com>
2025-05-23 01:02:40 +0900

    [Feat] LIFE UI 스크립트 연결

---
+++ Assets/Scripts/KDY/LifeUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LifeUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textmeshpro;
+
+    public void Start()
+    {
+        //Manager.Instance.observer.OnGameDataChange += OnChangeLife;
+        textmeshpro.text = "　x 100";
+        Debug.Log("적용된 텍스트: " + textmeshpro.text);
+    }
+
+
+    void OnChangeLife(DataCenter damage)
+    {
+        textmeshpro.text = $"x {damage.life}";
+    }
+}
