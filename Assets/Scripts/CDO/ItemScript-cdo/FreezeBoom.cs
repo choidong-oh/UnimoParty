43b2fc6c86b87ed6469517fead2538239410c1b4
choidong-oh <190792963+choidong-oh@users.noreply.github.com>
2025-05-28 13:39:57 +0900

    [Test] 테스트하기전 커밋

--- Assets/Scripts/CDO/ItemScript-cdo/FreezeBoom.cs
+++ Assets/Scripts/CDO/ItemScript-cdo/FreezeBoom.cs
@@ -1,11 +1,69 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FreezeBoom : MonoBehaviour
 {
 
+    private void Start()
+    {
+        StartCoroutine(wait());
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2);
+        Explode();
+    }
 
+    void Explode()
+    {
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, 3, Vector3.up, 10f, LayerMask.GetMask("Player", "Enemy", "Water"));
+        foreach (var hitobj in hits)
+        {
+            Debug.Log("빙결 폭탄");
+
+            GameObject target = hitobj.collider.gameObject;
+
+            //플레이어 
+            //왼손 : 움직임
+            //오른손 : 그랩, 트리거, 아이템
+            if (target.layer == LayerMask.NameToLayer("Player"))
+            {
+
+                if (target.TryGetComponent<JoystickController>(out JoystickController Player))
+                {
+                    Player.moveSpeed = 0;
+                    Debug.Log("플레이어 어름");
+                }
+
+
+            }
+
+            if (target.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                if (target.TryGetComponent<EnemyBase>(out EnemyBase Enemy))
+                {
+                    //ICommand command = null;
+                    //command = new FreezeCommand(Enemy, transform.position);
+                    //command.Execute();
+
+                    
+
+                    Debug.Log("enemy 어름");
+                }
+                if (target.TryGetComponent<TestEnemybase>(out TestEnemybase TestEnemy))
+                {
+                    TestEnemy.Freeze(transform.position);
+
+
+                    Debug.Log("TestEnemy 어름");
+                }
+
+            }
+
+
+
+        }
+    }
 
 
 
---
+++ Assets/Scripts/CDO/TestEnemybase.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemybase : EnemyBase
+{
+    public override void CsvEnemyInfo() { }
+
+    public override void Move(Vector3 direction)
+    {
+        Debug.Log("move호출");
+    }
+
+    public void Freeze(Vector3 direction)
+    {
+        Debug.Log("Freeze호출");
+    }
+
+
+}
