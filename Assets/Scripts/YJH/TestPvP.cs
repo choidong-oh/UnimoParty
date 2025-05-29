96d462c0e0132ebac22dd2cfa72e11c2867692ad
jonghyun109 <190792801+jonghyun109@users.noreply.github.com>
2025-05-29 18:28:34 +0900

    [feat] 기획자 , 개발자 방 나눔 빌드 용

--- Assets/Scripts/YJH/TestPvP.cs
+++ Assets/Scripts/YJH/TestPvP.cs
@@ -6,46 +6,106 @@ using UnityEngine.UI;
 
 public class TestPvP : MonoBehaviourPunCallbacks
 {
-    [SerializeField] Button TestStartBtn;
+    [Header("게임 스타트 버튼 들")]
+    [SerializeField] Button developerGameStartBtn;
+    [SerializeField] Button designerGameStartBtn;
 
+
+    [Header("방 입장 버튼 들")]
+    [SerializeField] Button developerJoinRoomBtn;
+    [SerializeField] Button designerJoinRoomBtn;
     void Start()
     {
-        TestStartBtn.interactable = false;
-        TestStartBtn.onClick.AddListener(() =>
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+
+        designerGameStartBtn.interactable = false;
+        designerJoinRoomBtn.interactable = false;
+
+        designerGameStartBtn.onClick.AddListener(() =>
         {
             if (PhotonNetwork.IsMasterClient)
             {
                 PhotonNetwork.LoadLevel(3);
             }
         });
+        designerJoinRoomBtn.onClick.AddListener(() => DesignerPVPJoinRoom());
+
+        developerGameStartBtn.interactable = false;
+        developerJoinRoomBtn.interactable = false;
+
+        developerGameStartBtn.onClick.AddListener(() =>
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.LoadLevel(3);
+            }
+        });
+        developerJoinRoomBtn.onClick.AddListener(() => DeveloperPVPJoinRoom());
     }
 
-    public void TestPVPButton()
+
+    //기획 방 생성
+    public void DesignerPVPCreatRoom()
     {
-        PhotonNetwork.AutomaticallySyncScene = true;
-        PhotonNetwork.ConnectUsingSettings(); // → OnConnectedToMaster 호출됨
+        PhotonNetwork.CreateRoom("Designer", new RoomOptions { MaxPlayers = 8 });
+        
     }
-
-    public override void OnConnectedToMaster()
+    //기획 방 들어가기
+    public void DesignerPVPJoinRoom()
     {
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinRoom("Designer");
     }
 
 
-    public override void OnJoinRandomFailed(short returnCode, string message)
+
+
+    //개발 방 생성
+    public void DeveloperPVPCreatRoom()
     {
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+        PhotonNetwork.CreateRoom("Developer", new RoomOptions { MaxPlayers = 8 });
+    }
+    //개발 방 들어가기
+    public void DeveloperPVPJoinRoom()
+    {
+        PhotonNetwork.JoinRoom("Developer");
     }
 
+
+
+    //방이 들어가면
     public override void OnJoinedRoom()
     {
-        if(PhotonNetwork.IsMasterClient)
+        //기획자 방 들어옴
+        int designerInRoom = 1;
+        if(PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.Name== "Designer")
         {
-            TestStartBtn.interactable = true;
+            designerGameStartBtn.interactable = true;
+            designerJoinRoomBtn.interactable = true;
+            designerGameStartBtn.GetComponentInChildren<TextMeshProUGUI>().text = $"{designerInRoom} 명 준비완료\n게임 시작";
         }
-        else
+        else if(PhotonNetwork.CurrentRoom.Name == "Designer")
         {
-            TestStartBtn.GetComponentInChildren<TextMeshProUGUI>().text = "IS READY";
+            designerInRoom++;
+            designerGameStartBtn.GetComponentInChildren<TextMeshProUGUI>().text = $"{designerInRoom} 명 입장";
         }
+
+
+
+        //개발 방 들어옴
+        int developerInRoom = 1;
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.Name== "Developer")
+        {
+            developerGameStartBtn.interactable = true;
+            developerJoinRoomBtn.interactable = true;
+            developerGameStartBtn.GetComponentInChildren<TextMeshProUGUI>().text = $"{developerInRoom} 명 준비완료\n게임 시작";
+        }
+        else if(PhotonNetwork.CurrentRoom.Name == "Developer")
+        {
+            developerInRoom++;
+            developerGameStartBtn.GetComponentInChildren<TextMeshProUGUI>().text = $"{developerInRoom} 명 입장";
+        }
+
+
     }
 }
