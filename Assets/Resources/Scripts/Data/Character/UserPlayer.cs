3be74ac3e29037ad439b069506c317ce5b0bf306
aruensia <aruensia@naver.com>
2025-05-30 17:35:25 +0900

    [Add] 상점, 우주선, 플레이어 CS 추가.
    SpaceShip 클래스 추가. itemData 추가. Potion 추가. IngameObserver 아이템 상호작용 함수 추가.

--- Assets/Resources/Scripts/Data/Character/UserPlayer.cs
+++ Assets/Resources/Scripts/Data/Character/UserPlayer.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class UserPlayer : MonoBehaviour
 {
-    FairyType fairyType;
-    public DataCenter gamedata = new DataCenter(100, 0, PlayerState.None);
+    public FairyType fairyType;
+    public DataCenter gamedata = new DataCenter(100,0,5000,PlayerState.None);
+    SpaceShipData SpaceShip;
 
     void Start()
     {
         Manager.Instance.observer.UserPlayer = this;
     }
-
 }
