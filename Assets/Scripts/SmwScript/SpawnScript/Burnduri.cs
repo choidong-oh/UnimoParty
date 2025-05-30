72c6a693c7d1e0db4ab0b56dc4fca254d23b171b
ldeemol <130666760+ldeemol@users.noreply.github.com>
2025-05-30 11:35:49 +0900

    [add]puwpuw 파티클 추가 , 몬스터 충돌판정 추가
    puwpuw 파티클 추가 , 몬스터 충돌판정 추가

--- Assets/Scripts/SmwScript/SpawnScript/Burnduri.cs
+++ Assets/Scripts/SmwScript/SpawnScript/Burnduri.cs
@@ -70,7 +70,7 @@ public class Burnduri : EnemyBase
 
 
             //Debug.Log(Manager.Instance.observer.UserPlayer.gamedata.life);
-            Manager.Instance.observer.HitPlayer(damage);
+            //Manager.Instance.observer.HitPlayer(damage);
             StopAllCoroutines();
             gameObject.SetActive(false);
         }
@@ -259,42 +259,7 @@ public class Burnduri : EnemyBase
 
     public override void Move(Vector3 direction)
     {
-        animator = GetComponent<Animator>();
-        myCollider = GetComponent<Collider>();
 
-        myCollider.enabled = false;
-
-        if (animator != null)
-        {
-            foreach (var clip in animator.runtimeAnimatorController.animationClips)
-            {
-                if (clip.name == "anim_01_MON001_Bduri_Appearance")
-                    appearanceClip = clip;
-                else if (clip.name == "anim_03_MON001_Bduri_Encounter")
-                    encounterClip = clip;
-                else if (clip.name == "anim_01_MON001_Bduri_Disappearance")
-                    disappearClip = clip;
-            }
-        }
-        // 컨트롤러에 등록된 모든 클립을 뒤져서 원하는 이름의 클립을 저장
-
-        if (appearanceClip == null || encounterClip == null || disappearClip == null)
-            Debug.LogWarning("Appearance 또는 Encounter 클립을 찾지 못했습니다.");
-
-        //한번만 찾을꺼임
-        if (players.Count == 0)
-        {
-            var objs = GameObject.FindGameObjectsWithTag("Player");
-            foreach (var obj in objs)
-            {
-                players.Add(obj.transform);
-            }
-        }
-
-        terrain = Terrain.activeTerrain;
-        isCharging = false;
-
-        StartCoroutine(GoBurnduri());
     }
 
   
