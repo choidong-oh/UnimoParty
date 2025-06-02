a751ab435928cbb521edd41c7a9aa1986e635ce7
choidong-oh <190792963+choidong-oh@users.noreply.github.com>
2025-06-02 14:23:51 +0900

    [ADD] VR 아이템 교체 스크립트 추가, PLAYER변경

--- Assets/Scripts/CDO/TestA.cs
+++ Assets/Scripts/CDO/TestA.cs
@@ -7,32 +7,21 @@ using UnityEngine.InputSystem;
 public class TestA : MonoBehaviour
 {
     [SerializeField]
-    private InputActionReference AInputActionReference;
     private InputActionReference BInputActionReference;
 
     private void OnEnable()
     {
-        AInputActionReference.action.Enable();
-        AInputActionReference.action.performed += ControllerA;
-
         BInputActionReference.action.Enable();
         BInputActionReference.action.performed += ControllerB;
     }
 
     private void OnDisable()
     {
-        AInputActionReference.action.performed -= ControllerA;
-        AInputActionReference.action.Disable();
-
         BInputActionReference.action.performed -= ControllerB;
         BInputActionReference.action.Disable();
     }
 
-    //아이템, 채집총 교체 a
-    private void ControllerA(InputAction.CallbackContext context)
-    {
-        Debug.Log("컨트롤 a버튼");
-    }
+
 
     //아이템 교체 b
     private void ControllerB(InputAction.CallbackContext context)
