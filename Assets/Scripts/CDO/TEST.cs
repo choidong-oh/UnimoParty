23d63a49240906d12020210ad40ce9f35b76920b
choidong-oh <qltjfeo@naver.com>
2025-06-12 00:01:50 +0900

    [ADD] 콜라이더 bounds 추가
    아이템 미리보기 느낌으로 추가했는데
    로테이션이 돌아가면 같이돌긴하지만 이상하게 돔 , 플레이어 자식객체로 넣어서 해야할듯

--- Assets/Scripts/CDO/TEST.cs
+++ Assets/Scripts/CDO/TEST.cs
@@ -1,48 +1,135 @@
 using UnityEngine;
 
+
+
+//bounds
+//bounds.center 프리팹의 정중앙
+//bounds.extents half프리팹의 2분의1의크기 xyz (반지름)
 public class TEST : MonoBehaviour
 {
     public GameObject cubePrefab;
     private GameObject previewCube;
 
-    public bool IsTuch = false;
-    public bool tuch2 = false;
+    public bool bool1 = false;
+    public bool bool2 = false;
+
+    public LayerMask layerMask;
 
     void Update()
     {
-        if (IsTuch == true)
+        //아이템 미리보기
+        if (bool1 == true)
         {
             if (previewCube == null)
             {
-                previewCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                previewCube = Instantiate(cubePrefab);
                 Destroy(previewCube.GetComponent<Collider>());
-                SetTransparent(previewCube, 0.3f);
+                SetTransparent(previewCube, 0.3f, Color.black);
             }
 
-            Vector3 spawnPos = transform.position + transform.forward * 2f;
-            previewCube.transform.position = spawnPos;
+            Vector3 Player = transform.position + transform.forward * 2f;
+
+            if (Physics.Raycast(Player, Vector3.down, out RaycastHit hit, 10f))
+            {
+                if (hit.collider.CompareTag("Ground"))
+                {
+                    Bounds bounds = previewCube.GetComponent<MeshRenderer>().bounds;
+                    Vector3 spawnPos = hit.point;
+                    spawnPos.y += bounds.extents.y;
+                    previewCube.transform.position = spawnPos;
+                }
+            }
+
+            int groundLayer = LayerMask.GetMask("Water"); 
+            int mask = ~groundLayer;//그라운드만 제외 비트연산
+            // 나중에 레이어마스크 인스펙터창에서 빼야할듯 public LayerMask layerMask; 
+            Bounds previewBounds = previewCube.GetComponent<Renderer>().bounds;
+
+            bool isBlocked = Physics.CheckBox (previewBounds.center,
+                                               previewBounds.extents,
+                                               previewCube.transform.rotation,
+                                               mask);
+
+            if (isBlocked == true)
+            {
+                Debug.Log("겹치는 오브젝트 있음!");
+                SetTransparent(previewCube, 0.3f, Color.red);
+            }
+            else
+            {
+                SetTransparent(previewCube, 0.3f, Color.green);
+            }
+
         }
-        else if (IsTuch == false)
+        else if (bool1 == false)
         {
+            //기존 프리팹 삭제
             Destroy(previewCube);
+
         }
 
-        if (tuch2 == true)
+
+
+        //아이템 생성
+        if (bool2 == true)
         {
-            tuch2 = false;
-            Vector3 spawnPos = transform.position + transform.forward * 2f;
-            Instantiate(cubePrefab, spawnPos, Quaternion.identity);
+            bool2 = false;
+
+            if (CanPlace() == false)
+            {
+                Instantiate(cubePrefab, GroundPos(), Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("겹쳐서 배치 불가!");
+            }
         }
+
+
+
     }
 
-    void SetTransparent(GameObject obj, float alpha)
+    //설치 가능한지
+    bool CanPlace()
+    {
+        if (previewCube == null) { return false; }
+
+        Bounds previewBounds = previewCube.GetComponent<Renderer>().bounds;
+
+        bool isBlocked = Physics.CheckBox(previewBounds.center, previewBounds.extents, previewCube.transform.rotation);
+
+        return isBlocked;
+    }
+
+    //바닥 포지션
+    Vector3 GroundPos()
+    {
+        Vector3 Player = transform.position + transform.forward * 2f;
+
+        if (Physics.Raycast(Player, Vector3.down, out RaycastHit hit, 10f))
+        {
+            if (hit.collider.gameObject.tag == "Ground")
+            {
+                Bounds bounds = previewCube.GetComponent<MeshRenderer>().bounds;
+                Vector3 spawnPos = hit.point;
+                spawnPos.y += bounds.extents.y;
+                return spawnPos;
+            }
+        }
+
+        return Player;
+    }
+
+
+    //머티리얼 색변환
+    void SetTransparent(GameObject obj, float alpha, Color color)
     {
         var renderer = obj.GetComponent<MeshRenderer>();
 
         var mat = renderer.materials[0];
 
-        mat.color = new Color(1, 1, 1, alpha);
-
+        color.a = alpha;
+        mat.color = color;
         renderer.material = mat;
     }
 
