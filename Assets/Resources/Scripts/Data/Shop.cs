0daff7d692696d68e77422156ef0a875b840b559
aruensia <aruensia@naver.com>
2025-06-11 16:34:19 +0900

    [Add] 사운드 매니저 추가.
    상점 스크립트 임시 추가, csv 로더 수정

--- Assets/Resources/Scripts/Data/Shop.cs
+++ Assets/Resources/Scripts/Data/Shop.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
-    
+    Dictionary<string, List<InterfaceMethod.TableData>> dataDic;
+    ItemData selectedItem;
+    public Sprite shipImage;
+    int selectMenuNum;
 
     private void Start()
     {
@@ -14,16 +18,41 @@ public class Shop : MonoBehaviour
     void GetItemData()
     {
         var tempdatas = Manager.Instance.dataLoader.data;
+        dataDic = tempdatas;
     }
 
-    void ShopItemList()
+    void ShowShopItemList()
     {
+        switch(selectMenuNum)
+        {
+            case 0:
+              
+                for (int i = 0; i < dataDic["Enemy"].Count; i++)
+                {
+                    var temp = dataDic["Enemy"][i];
 
+                }
+                break;
+
+            case 1:
+
+                break;
+            
+            case 2:
+
+                break;
+
+        }
     }
 
     void SelectItem()
     {
+        // 내가 트리거든 버튼이든 뭔가 눌렀을 때, 반환되는 자료를 저장. 
+    }
 
+    void ButItemButton()
+    {
+        Manager.Instance.observer.BuyItem(selectedItem);
     }
 
 }
