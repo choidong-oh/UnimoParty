9732069921bfa2b0c4cc77b042a1b6634fc028f1
choidong-oh <190792963+choidong-oh@users.noreply.github.com>
2025-06-11 18:50:22 +0900

    [Add] 아이템 미리보기 bounds 넣기전 만들었음
    나중에 bounds로 콜라이더 체크해서 못넣으면 못넣게 예외처리 해야댐

--- Assets/Scripts/CDO/TEST.cs
+++ Assets/Scripts/CDO/TEST.cs
@@ -2,20 +2,48 @@ using UnityEngine;
 
 public class TEST : MonoBehaviour
 {
-    public GameObject boundingBoxPrefab; // 투명한 큐브 프리팹
-    private GameObject boxInstance;
+    public GameObject cubePrefab;
+    private GameObject previewCube;
 
-    void Start()
+    public bool IsTuch = false;
+    public bool tuch2 = false;
+
+    void Update()
     {
-        Collider col = GetComponent<Collider>();
-        if (col != null && boundingBoxPrefab != null)
+        if (IsTuch == true)
         {
-            Bounds b = col.bounds;
+            if (previewCube == null)
+            {
+                previewCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Destroy(previewCube.GetComponent<Collider>());
+                SetTransparent(previewCube, 0.3f);
+            }
 
-            boxInstance = Instantiate(boundingBoxPrefab, b.center, Quaternion.identity);
-            boxInstance.transform.localScale = b.size;
-
+            Vector3 spawnPos = transform.position + transform.forward * 2f;
+            previewCube.transform.position = spawnPos;
         }
+        else if (IsTuch == false)
+        {
+            Destroy(previewCube);
+        }
+
+        if (tuch2 == true)
+        {
+            tuch2 = false;
+            Vector3 spawnPos = transform.position + transform.forward * 2f;
+            Instantiate(cubePrefab, spawnPos, Quaternion.identity);
+        }
+    }
+
+    void SetTransparent(GameObject obj, float alpha)
+    {
+        var renderer = obj.GetComponent<MeshRenderer>();
+
+        var mat = renderer.materials[0];
+
+        mat.color = new Color(1, 1, 1, alpha);
+
+        renderer.material = mat;
     }
 
 
