1c7b14b3c7047d668163a88907473dd55f865ffc
jonghyun109 <190792801+jonghyun109@users.noreply.github.com>
2025-06-19 14:17:26 +0900

    [feat] 상점 기능 추가(미완성)

--- Assets/Scripts/YJH/ShopManager.cs
+++ Assets/Scripts/YJH/ShopManager.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
@@ -8,13 +11,9 @@ public class ShopManager : MonoBehaviour
     private int ingameMoney;
     [SerializeField] TextMeshProUGUI money1;
 
-    [Header("몸 프리팹")]
-    [SerializeField] GameObject riBeePrefab;
-    [SerializeField] GameObject catPrefab;
-
-    [Header("스폰 포지션")]
-    [SerializeField] Transform body;
-    [SerializeField] Transform spaceShip;
+    [Header("프리팹들")]
+    [SerializeField] GameObject[] characters;
+    [SerializeField] GameObject[] ships;
 
     [Header("스크롤 뷰들")]
     [SerializeField] GameObject[] scrollVeiw;
@@ -22,15 +21,18 @@ public class ShopManager : MonoBehaviour
     [Header("토글 들")]
     [SerializeField] Toggle[] viewToggles;
 
+    public bool isBuy=false;
+    [SerializeField] SpaceShip[] spaceShipD;
 
-    private SpaceShipData selectedSpaceShipData;
 
-    private void Start()
+    [SerializeField] GameObject sellPanel;
+    private IEnumerator Start()
     {
         ingameMoney = Manager.Instance.observer.UserPlayer.gamedata._money;
         money1.text = ingameMoney.ToString();
 
 
+        sellPanel.SetActive(false);
 
         for (int i = 0; i < viewToggles.Length; i++)
         {
@@ -40,8 +42,42 @@ public class ShopManager : MonoBehaviour
                 break;
             }
         }
+
+        foreach (SpaceShip d in spaceShipD)
+        {
+            Debug.Log(d.SpaceShipData._isbuy);
+
+            Task<bool> loadTask = FirebaseAuthMgr.Instance.LoadUserDataAsync<bool>(FirebaseAuthMgr.user.DisplayName, d.gameObject.name);
+            yield return new WaitUntil(() => loadTask.IsCompleted);
+
+            d.SpaceShipData._isbuy = loadTask.Result;
+
+            if (d.SpaceShipData._isbuy)
+            {
+
+                // 구매된 경우 로직
+            }
+        }
+
+
+
+
     }
 
+    public void CharacterPreview(int index)
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            characters[i].SetActive(i == index);
+        }
+    }
+    public void ShipPreview(int index)
+    {
+        for (int i = 0; i < ships.Length; i++)
+        {
+            ships[i].SetActive(i == index);
+        }
+    }
     public void ViewActive(int activeIndex)
     {
         for (int i = 0; i < scrollVeiw.Length; i++)
@@ -50,25 +86,26 @@ public class ShopManager : MonoBehaviour
         }
     }
 
-    public void OnShipSelect(SpaceShipData selectedShip)
-    {        
 
-        selectedSpaceShipData = selectedShip;
-        buyButton.gameObject.SetActive(true);
-        buyButton.onClick.AddListener(BuyShipButton);
-    }
-
-    public void BuyShipButton()
+    public void BuyShipButton(SpaceShip selectedShip)
     {
-        Manager.Instance.observer.BuyShip(selectedSpaceShipData);
-
-
+        buyButton.onClick.AddListener(() =>BuyShip(selectedShip));
+        selectedShip.shipnamek = selectedShip.gameObject.name;
+        sellPanel.SetActive(true);
+        //buyButton.gameObject.SetActive(false);
+    }
+    public void BuyShip(SpaceShip selectedShip)
+    {
+        Manager.Instance.observer.BuyShip(selectedShip.SpaceShipData);
+        
         ingameMoney = Manager.Instance.observer.UserPlayer.gamedata._money;
         money1.text = ingameMoney.ToString();
+        Debug.Log("정보 저장 중");
+        StartCoroutine(FirebaseAuthMgr.Instance.SaveUserData(FirebaseAuthMgr.user.DisplayName, selectedShip.shipnamek, selectedShip.SpaceShipData._isbuy = true));
 
-        FirebaseAuthMgr.Instance.SaveUserData(FirebaseAuthMgr.user.DisplayName, "ShipData", selectedSpaceShipData.Name);
-        selectedSpaceShipData = null;
-        buyButton.gameObject.SetActive(false);
+        spaceShipD = null;
+        sellPanel.SetActive(false);
     }
+
 
 }
