0bbe605162bf647a5a33bbd74c774911861027f2
ldeemol <130666760+ldeemol@users.noreply.github.com>
2025-06-23 15:24:18 +0900

    [Fixed]레이저, 폭탄 포툰 수정
    레이저, 폭탄 포툰 수정

--- Assets/Scripts/SmwScript/SpawnScript/MonsterScript/Lazercoll.cs
+++ Assets/Scripts/SmwScript/SpawnScript/MonsterScript/Lazercoll.cs
@@ -1,18 +1,25 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lazercoll : MonoBehaviour
+public class Lazercoll : MonoBehaviourPunCallbacks
 {
     int damage; 
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!photonView.IsMine) return;
         if (other.gameObject.tag == "Player")
         {
 
             damage = 1;
-            Manager.Instance.observer.HitPlayer(damage);
+            var otherPV = other.GetComponent<PhotonView>();
+            if (otherPV != null && otherPV.Owner != null)
+            {
+                // 데미지 전용 RPC
+                photonView.RPC("HitPlayerRPC", otherPV.Owner, damage + 1);
+            }
             Debug.Log(Manager.Instance.observer.UserPlayer.gamedata.life);
         }
 
