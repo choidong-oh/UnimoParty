76ee001189e2620995126ebc671b6410529d3029
KIMDONGYEON8406 <kdys8599@naver.com>
2025-06-25 17:41:16 +0900

    [Fix] Trigger > Collsion

--- Assets/Scripts/KDY/FairyDeliveryTrigger.cs
+++ Assets/Scripts/KDY/FairyDeliveryTrigger.cs
@@ -3,9 +3,20 @@ using Photon.Pun;
 
 public class FairyDeliveryTrigger : MonoBehaviourPun
 {
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player")&&photonView.IsMine)
+    //    {
+    //        Manager.Instance.observer.AddScore();
+    //        Debug.Log(" 플레이어가 AUBE에 닿음 페어리 반납");
+    //        Manager.Instance.observer.DeliveryFairy();
+
+    //    }
+    //}
+
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.CompareTag("Player")&&photonView.IsMine)
+        if (collision.gameObject.CompareTag("Player") && photonView.IsMine)
         {
             Manager.Instance.observer.AddScore();
             Debug.Log(" 플레이어가 AUBE에 닿음 페어리 반납");
