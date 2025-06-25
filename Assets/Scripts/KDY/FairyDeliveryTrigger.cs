b287c30f8902f6ab1f8823df6647e176f1bd141f
KIMDONGYEON8406 <kdys8599@naver.com>
2025-06-25 18:03:41 +0900

    [Feat] 수정

--- Assets/Scripts/KDY/FairyDeliveryTrigger.cs
+++ Assets/Scripts/KDY/FairyDeliveryTrigger.cs
@@ -3,20 +3,9 @@ using Photon.Pun;
 
 public class FairyDeliveryTrigger : MonoBehaviourPun
 {
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Player")&&photonView.IsMine)
-    //    {
-    //        Manager.Instance.observer.AddScore();
-    //        Debug.Log(" 플레이어가 AUBE에 닿음 페어리 반납");
-    //        Manager.Instance.observer.DeliveryFairy();
-
-    //    }
-    //}
-
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player") && photonView.IsMine)
+        if (other.CompareTag("Player") && other.TryGetComponent<PhotonView>(out var dd) && dd.IsMine)
         {
             Manager.Instance.observer.AddScore();
             Debug.Log(" 플레이어가 AUBE에 닿음 페어리 반납");
@@ -24,4 +13,18 @@ public class FairyDeliveryTrigger : MonoBehaviourPun
 
         }
     }
+
+    ////충돌 처리용
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player") && photonView.IsMine)
+    //    {
+    //        Manager.Instance.observer.AddScore();
+    //        Debug.Log(" 플레이어가 AUBE에 닿음 페어리 반납");
+    //        Manager.Instance.observer.DeliveryFairy();
+
+//    }
 }
+
+
+
